a8b4f8d3820e365f8c166e828d3953e3937f88c2
Gustavo Mori <gustavo.mori.peixoto@usp.br>
2020-08-27 23:00:03 -0300

    add feature to create transactions and list transactions repositories

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,28 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type}: RequestDTO): Transaction {
+
+    const { total } = this.transactionsRepository.getBalance()
+
+    if (type === 'outcome' && total < value){
+      throw Error('You dont have money enough')
+    }
+    if (!['income', 'outcome'].includes(type)){
+      throw Error('Type is invalid')
+    }
+    if (value < 0){
+      throw Error('Value must be positive')
+    }
+    if (typeof title !== 'string'){
+      throw Error('Title must be a text')
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+    return transaction;
   }
 }
 
